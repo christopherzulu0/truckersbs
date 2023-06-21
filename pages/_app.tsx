21938e2a67620bbb7f291597efc19a1b7f0cf80c
christopherzulu0 <christopherzulu04@gmail.com>
2023-06-21 12:05:43 +0200

    second push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-// import '@/styles/globals.css'
+import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
+import { RecoilRoot } from "recoil";
+import Layout from "../Components/Layout";
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider><Component {...pageProps} /></ChakraProvider>
+  return   <RecoilRoot><ChakraProvider><Layout><Component {...pageProps} /></Layout></ChakraProvider></RecoilRoot>
 }
