ba2e1be7c601681bcddf675f0c028316fb867169
Desire <desireirankundawilliams@gmail.com>
2023-06-29 09:45:41 +0200

    validated the report

---
+++ Components/validationSchema/report.js
@@ -0,0 +1,31 @@
+import * as yup from "yup";
+
+export const reportSchema = yup.object({
+    type: yup
+    .string("Enter your type ")
+    .required("type is required"),
+    traffic: yup
+    .string("Enter traffic percentage")
+    .required("traffic percentage is required"),
+    route: yup
+    .string("Enter route")
+    .required("route required"),
+    condition: yup
+    .string("Enter your condition of the place")
+    .required("condition of the place is required"),
+    attachment: yup
+    .string("Select image")
+    .required("image is required"),
+    description: yup
+    .string("Enter your description")
+    .required("Description is required")
+});
+
+export const reportValues = {
+  type: "",
+  traffic: "",
+  route: "",
+  condition: "",
+  attachment: "",
+  description: "",
+};
--- pages/Analytics.tsx
+++ pages/Analytics.tsx
@@ -1,45 +1,217 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    Button
-  } from '@chakra-ui/react'
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Box,
+  Center,
+  ButtonGroup,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  HStack,
+  Select,
+  Textarea
+} from '@chakra-ui/react'
+import { useFormik } from 'formik';
+import { reportValues, reportSchema } from "../Components/validationSchema/report"
 
 
 export default function Analytics() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const formik = useFormik({
+    initialValues: reportValues,
+    validationSchema: reportSchema,
+    onSubmit: async (values) => {
+      try {
+        // const directions = await addDoc(collection(db, "directions"), {
+        //   from: values.from,
+        //   destination: values.destination,
+        //   startTime: values.startTime,
+        //   firstStop: values.firstStop,
+        // });
+        // console.log("values directions", directions);
+        alert(JSON.stringify(values, null, 2));
+      } catch (error) {
+        console.log("error", error);
+      }
+    },
+  });
+
 
   return (
-     <>
-    <Button 
-    bgColor={'blue.400'}
-    color={'white'}
-    ml={1}
-    onClick={onOpen}>Analytics</Button>
+    <>
+      <Button
+        bgColor={'blue.400'}
+        color={'white'}
+        ml={1}
+        onClick={onOpen}>Analytics</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Analytics
+          </ModalBody>
+          <HStack spacing={2} justifyContent="space-between">
+            <Box flexGrow={1}>
+              <FormControl
+                isInvalid={
+                  !!formik.errors.type && formik.touched.type
+                }
+              >
+                <FormLabel htmlFor="Type">
+                  Type
+                  <Select
+                    name="type"
+                    onChange={formik.handleChange}
+                    value={formik.values.type}
+                  >
+                    <option value="option1">Road</option>
+                    <option value="option2">Accident</option>
+                    <option value="option3">Rainly</option>
+                  </Select>
+                  <FormErrorMessage>
+                    {formik.errors.type}
+                  </FormErrorMessage>
+                </FormLabel>
+              </FormControl>
+            </Box>
+            <Box flexGrow={1}>
+              <FormControl
+                isInvalid={
+                  !!formik.errors.traffic && formik.touched.traffic
+                }
+              >
+                <FormLabel htmlFor="Traffic">
+                  Traffic
+                  <Select
+                    name="traffic"
+                    onChange={formik.handleChange}
+                    value={formik.values.traffic}
+                  >
+                    <option value="option1">10%</option>
+                    <option value="option1">20%</option>
+                    <option value="option1">40%</option>
+                    <option value="option2">60%</option>
+                    <option value="option3">80%</option>
+                    <option value="option3">100%</option>
+                  </Select>
+                  <FormErrorMessage>
+                    {formik.errors.traffic}
+                  </FormErrorMessage>
+                </FormLabel>
+              </FormControl>
+            </Box>
+          </HStack>
 
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          Analytics
-        </ModalBody>
+          <HStack spacing={2} justifyContent="space-between">
+            <Box flexGrow={1}>
+              <FormControl
+                isInvalid={
+                  !!formik.errors.route && formik.touched.route
+                }
+              >
+                <FormLabel htmlFor="Route">
+                  Route
+                  <Select
+                    name="route"
+                    onChange={formik.handleChange}
+                    value={formik.values.route}
+                  >
+                    <option value="option1">Montreal - Quebec</option>
+                    <option value="option2">Montreal - Ontario</option>
+                    <option value="option3">Quebec - Toronto</option>
+                    <option value="option3">Montreal - Quebec</option>
+                  </Select>
+                  <FormErrorMessage>
+                    {formik.errors.route}
+                  </FormErrorMessage>
+                </FormLabel>
+              </FormControl>
+            </Box>
+            <Box flexGrow={1}>
+              <FormControl
+                isInvalid={
+                  !!formik.errors.condition && formik.touched.condition
+                }
+              >
+                <FormLabel htmlFor="Condition">
+                  Condition
+                  <Select
+                    name="condition"
+                    onChange={formik.handleChange}
+                    value={formik.values.condition}
+                  >
+                    <option value="option1">Good</option>
+                    <option value="option2">Bad</option>
+                    <option value="option3">Better</option>
+                  </Select>
+                  <FormErrorMessage>
+                    {formik.errors.condition}
+                  </FormErrorMessage>
+                </FormLabel>
+              </FormControl>
+            </Box>
+          </HStack>
+          <Box flexGrow={1}>
+            <FormControl
+              isInvalid={!!formik.errors.attachment && formik.touched.attachment}
+            >
+              <FormLabel htmlFor="Attachment">
+                Attachment
+                <Input
+                  name="attachment"
+                  type="file"
 
-        <ModalFooter>
-          <Button colorScheme='blue' mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button variant='ghost'>Secondary Action</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  </>
+                  onChange={formik.handleChange}
+                  value={formik.values.attachment}
+                />
+                <FormErrorMessage>
+                  {formik.errors.attachment}
+                </FormErrorMessage>
+              </FormLabel>
+            </FormControl>
+          </Box>
+          <Box flexGrow={1}>
+            <FormControl
+              isInvalid={!!formik.errors.description && formik.touched.description}
+            >
+              <FormLabel htmlFor="Description">
+                Description
+                <Textarea name="description" placeholder='Here is a sample placeholder' onChange={formik.handleChange}
+                  value={formik.values.description} />
+                <FormErrorMessage>
+                  {formik.errors.description}
+                </FormErrorMessage>
+              </FormLabel>
+            </FormControl>
+          </Box>
+          <ModalFooter>
+            <Box>
+              <Center>
+                <ButtonGroup ml={4}>
+                  <Button onClick={formik.handleSubmit} colorScheme="blue" type="submit">
+                    Report
+                  </Button>
+                  <Button colorScheme="blue" onClick={onClose} type="submit">
+                    Cancel
+                  </Button>
+                </ButtonGroup>
+              </Center>
+            </Box>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
